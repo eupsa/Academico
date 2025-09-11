3d50cfde29aaaeb2ba3cd8a476ff5aa61a5c3150
Pedro Araujo <106033591+eupsa@users.noreply.github.com>
2025-09-11 16:06:14 -0300

    Update MultiplosDe3.py

--- Python/MultiplosDe3.py
+++ Python/MultiplosDe3.py
@@ -2,7 +2,6 @@ i = 0
 soma = 0
 
 for i in range(1, 501):
-    if i % 3 == 0:
-        if i % 2 != 0:
+    if (i % 3 == 0 and i % 2 != 0):
             soma+=i
-print(soma)
+print(f"Resultado: {soma}")
